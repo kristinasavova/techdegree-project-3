aa6eb5c8a7c3986c15b008a8e80408e3623c5054
Kristina <ccthw@protonmail.com>
2020-03-11 21:39:33 +0200

    updated script.js

--- js/script.js
+++ js/script.js
@@ -26,5 +26,53 @@ selectJob.addEventListener('change', () => {
     } 
 });
 
+// Select and hide colors in the 'color' drop down menu   
 
+const colors = document.querySelectorAll('#color option'); 
+for (let i = 0; i < colors.length; i ++) {
+    colors[i].style.display = 'none'; 
+}  
+
+// Update the 'color' field to read 'please select a T-shirt theme' 
+
+const selectColor = document.querySelector('#color'); // select the parent node 
+const selectThemeOption = document.createElement('option'); // create 'select theme' option  
+selectThemeOption.textContent = 'Please select a T-shirt theme'; 
+selectThemeOption.setAttribute('selected', true); // update the 'color' field  
+selectColor.appendChild(selectThemeOption); // append new option to the select element 
+selectThemeOption.style.display = 'none'; // make 'select theme' option invisible
+
+// Hide the 'select theme' option element in the 'design' menu 
+
+const designs = document.querySelectorAll('#design option'); // select all design options 
+designs[0].style.display = 'none'; // hide the first one
+
+// Select the select element for the 'design' menu and set event listener on it
+
+const selectDesign = document.querySelector('#design');
+
+selectDesign.addEventListener('change', (event) => {
+    for (i = 0; i < designs.length; i ++) {
+        let designOption = event.target.value;  
+        if (designOption === 'js puns') { // if 'js puns' is selected, hide 'heart js' options
+            selectThemeOption.removeAttribute('selected'); 
+            colors[0].setAttribute('selected', true); // update the 'color' field
+            for (let i = 0; i < 3; i ++) { 
+                colors[i].style.display = ''; 
+            }
+            for (let i = 3; i <= 5; i ++) {
+                colors[i].style.display = 'none'; 
+            } 
+        } else if (designOption === 'heart js') { // if 'heart js' is selected, hide 'js puns' options
+            selectThemeOption.removeAttribute('selected'); 
+            colors[3].setAttribute('selected', true); // update the 'color' field 
+            for (let i = 0; i < 3; i ++) { 
+                colors[i].style.display = 'none'; 
+            }
+            for (let i = 3; i <= 5; i ++) {
+                colors[i].style.display = ''; 
+            } 
+        }
+    }
+});
 
